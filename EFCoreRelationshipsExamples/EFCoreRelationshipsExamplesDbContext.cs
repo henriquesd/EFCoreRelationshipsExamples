fb1ed48c992159851ae2ea5adb2919cb7f642439
henriquesd <hsd.foz@gmail.com>
2020-08-21 18:33:37 +0200

    Created project

---
+++ EFCoreRelationshipsExamples/EFCoreRelationshipsExamplesDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreRelationshipsExamples
+{
+    public class EFCoreRelationshipsExamplesDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Server=(localdb)\MSSQLLocalDB;Database=EFCoreRelationshipsExamples;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+    }
+}
---
+++ EFCoreRelationshipsExamples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EFCoreRelationshipsExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
